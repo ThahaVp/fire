0c6b4d14f8da271e798c8f275ac82b327daea950
Thaha Vp <thahaponnani@gmail.com>
2022-05-25 09:50:19 +0530

    api update

--- routes/bytes.js
+++ routes/bytes.js
@@ -650,6 +650,15 @@ router.post('/addDeliveryAddress', (req, res) => {
     ln: req.body.ln
   }
 
+  var keys = Object.keys(data)
+  for (var i=0; i<keys.length; i++)
+  {
+    if (data.keys[i] == null)
+    {
+      data.key[i] = ""
+    }
+  }
+
   if (aid != "")
   {
     bytesHelper.updateAddress(data, uid).then((responce => {
