6efa3d25daba4f444c734b133141b9eb064232ed
Thaha Vp <thahaponnani@gmail.com>
2022-06-10 11:55:25 +0530

    cron updated

--- cornJob.js
+++ cornJob.js
@@ -3,20 +3,76 @@ var admin = require("firebase-admin");
 var serviceAccount = require("./delivery-58fd5-firebase-adminsdk-pxhyn-f6c803d34a.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://delivery-58fd5.firebaseio.com"
-  });
-
-const job = cron.schedule("1 1 19 * * *", () => {
-
-    console.log("working")
-    const db = admin.database();
-  const ref = db.ref('Area/ponnani/shop/fruitify/status');
-  ref.set("open").then(function () {
-    console.log("done")
-  }).catch(function (error) {
-  console.log("error " + error)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://delivery-58fd5.firebaseio.com"
 });
+
+let arr = [
+  {
+    ref: "food feast",
+    area: "ponnani",
+    hour: 15,
+    minute: 1
+  },
+  {
+    ref: "harbour heritage",
+    area: "ponnani",
+    hour: 12,
+    minute: 30
+  },
+  {
+    ref: "ikkayis kuzhimandhi",
+    area: "ponnani",
+    hour: 11,
+    minute: 1
+  },
+  {
+    ref: "jawas restaurant",
+    area: "ponnani",
+    hour: 16,
+    minute: 1
+  },
+  {
+    ref: "juicy",
+    area: "ponnani",
+    hour: 12,
+    minute: 1
+  },
+  {
+    ref: "modern foods",
+    area: "ponnani",
+    hour: 11,
+    minute: 30
+  },
+  {
+    ref: "scoopso",
+    area: "ponnani",
+    hour: 11,
+    minute: 1
+  },
+]
+
+
+const job = cron.schedule("1 1,30 11,12,15,16 * * *", () => {
+
+  let ts = Date.now();
+  let date_ob = new Date(ts);
+  let minutes = date_ob.getMinutes()
+  let hour = date_ob.getHours()
+
+  const db = admin.database();
+  for (var i = 0; i < arr.length; i++)
+  {
+    let obj = arr[i]
+    if (hour == obj.hour) {
+      const ref = db.ref('Area/' + obj.area + '/shop/' + obj.ref + '/status');
+      ref.set("open").then(function () {
+        console.log("done")
+      }).catch(function (error) {
+        console.log("error " + error)
+      });
+    }
+  }
 
 });
 
@@ -25,4 +81,4 @@ const job = cron.schedule("1 1 19 * * *", () => {
 // });
 
 //   secondJob.start()
-  job.start()
+job.start()
